874343ecac184c06c8e2ad5f1cf37b8089e27479
anish khan <anish.khan@cognerate.ai-at-553856284116>
2025-09-23 17:28:44 +0530

    changes

--- app/Http/Controllers/api/FollowApiController.php
+++ app/Http/Controllers/api/FollowApiController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Follow;
 use App\Models\User;
 use App\Models\AppNotification;
+use App\Models\UserSubscriptions;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
